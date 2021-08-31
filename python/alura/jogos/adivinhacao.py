e6636c49352f20fb8f7aa3ffe90c243832acf959
Marcus William <marcus.william@tjpa.jus.br>
2021-08-30 21:25:59 -0300

    Lidando com a entrada do usuário

---
+++ python/alura/jogos/adivinhacao.py
@@ -0,0 +1,18 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto=42
+
+chute_string = input("Digite o seu palpite: ")
+
+print("Você digitou",chute_string)
+
+chute = int(chute_string)
+
+if (numero_secreto == chute):
+    print("você acertou")
+else:
+    print("você errou")
+
+print("fim do jogo")
