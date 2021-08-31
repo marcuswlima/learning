07c7cf9047d8f39feb7911b746985f08e23cf07c
Marcus William <marcus.william@tjpa.jus.br>
2021-08-31 10:30:03 -0300

    05.Iterando de maneira diferente - for com continue

--- python/alura/jogos/adivinhacao.py
+++ python/alura/jogos/adivinhacao.py
@@ -7,11 +7,14 @@ total_de_tentativas = 3
 
 
 for rodada in range(1,total_de_tentativas + 1):
-    #print("Tentativa", rodada, " de ", total_de_tentativas)
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_string = input("Digite o seu palpite: ")
+    chute_string = input("Digite um número entre 1 e 100: ")
     print("Você digitou", chute_string)
     chute = int(chute_string)
+
+    if (chute < 1 or chute > 100 ):
+        print("**Você deve digitar um número entre 1 e 100 !!!**0")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -19,6 +22,7 @@ for rodada in range(1,total_de_tentativas + 1):
 
     if acertou:
         print("você acertou")
+        break
     else:
         if maior:
             print("você errou! O seu chute foi maior que o número secreto.")
