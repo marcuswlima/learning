a63edf365a066638043bfbd6cb3d0a7e6dfc0671
marcus_umarizal <marcus.william@tjpa.jus.br>
2021-08-31 00:19:37 -0300

    04. A sequencia do jogo - Formatação de Strings

--- python/alura/jogos/adivinhacao.py
+++ python/alura/jogos/adivinhacao.py
@@ -6,22 +6,23 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
-    print("Tentativa", rodada, " de ", total_de_tentativas)
+while rodada <= total_de_tentativas:
+    #print("Tentativa", rodada, " de ", total_de_tentativas)
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_string = input("Digite o seu palpite: ")
-    print("Você digitou",chute_string)
+    print("Você digitou", chute_string)
     chute = int(chute_string)
 
     acertou = chute == numero_secreto
-    maior   = chute > numero_secreto
-    menor   = chute < numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-    if (acertou):
+    if acertou:
         print("você acertou")
     else:
-        if(maior):
+        if maior:
             print("você errou! O seu chute foi maior que o número secreto.")
-        elif(menor):
+        elif menor:
             print("você errou! O seu chute foi menor que o número secreto.")
 
     rodada = rodada + 1
