586af32ac812f6b6b9223f4e3da583a6e7e48c36
marcus_umarizal <marcus.william@tjpa.jus.br>
2021-09-01 11:57:47 -0300

    02. Manipulado string

--- python/alura/jogos/forca.py
+++ python/alura/jogos/forca.py
@@ -5,6 +5,25 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper()==letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(chute,index))
+            index=index+1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if __name__ == "__main__":
