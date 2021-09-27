95f6ec14ef9c6443807bb88a8c6b67aad992c6ae
marcus_umarizal <marcus.william@tjpa.jus.br>
2021-09-27 12:44:35 -0300

    exe03_12

--- c++/Deitel/src/cap03/exe03_12.cpp
+++ c++/Deitel/src/cap03/exe03_12.cpp
@@ -20,7 +20,7 @@ double calculateCharges (double in_horas);
 
 int main()
 {
-    double t1, t2, t3, totalHoras=0, totalTaxa=0, totalCalculateCharges=0;
+    double t1, t2, t3, totalHoras=0, totalCalculateCharges=0, dCalculateCharges;
 
     cout << "Inform tempo carro 1: ";
     cin >> t1;
@@ -30,34 +30,28 @@ int main()
     cin >> t3;
 
     cout << endl;
-    cout << "Carro\tHoras\tTaxa Cobrada" << endl;
+    cout << "Carro\tHoras\tTaxaCobrada" << setprecision(2) <<  setiosflags( ios::fixed | ios::showpoint) << endl;
 
-    // carro 1
-    {
-        totalHoras += t1;
-        totalCalculateCharges += calculateCharges(t1);
-        //cout << "1\t" << t1 << "\t" << calculateCharges(t1) << endl ;
+    dCalculateCharges = calculateCharges(t1);
+    cout << "1\t" << t1 << "\t" << dCalculateCharges << endl;
 
-    }
-    // carro 2
-    {
-        totalHoras += t2;
-        totalCalculateCharges += calculateCharges(t2);
-        //cout << "2\t" << t2 << "\t" << calculateCharges(t2) << endl ;
+    totalHoras += t1;
+    totalCalculateCharges += dCalculateCharges;
 
-    }
+    dCalculateCharges = calculateCharges(t2);
+    cout << "2\t" << t2 << "\t" << dCalculateCharges << endl;
+    totalHoras += t2;
+    totalCalculateCharges += dCalculateCharges;
 
-    // carro 3
-    {
-        totalHoras += t3;
-        totalCalculateCharges += calculateCharges(t3);
-        //cout << "3\t" << t3 << "\t" << calculateCharges(t3) << endl ;
+    dCalculateCharges = calculateCharges(t3);
+    cout << "3\t" << t3 << "\t"  << dCalculateCharges << endl;
+    totalHoras += t3;
+    totalCalculateCharges += dCalculateCharges;
 
-    }
+    cout << "Total\t" << totalHoras << "\t" << totalCalculateCharges << endl;
 
-    // Total
-    //cout << "Total\t" << totalHoras << "\t" << totalCalculateCharges << endl ;
 //--------------------------------------------
+
     cout << endl << endl ;
     return 0;
 }
@@ -67,19 +61,17 @@ double calculateCharges (double in_horas) {
     int    iHorasTaxaMinima=3;
     double dTaxaMinima=2, dResposta;
 
-    cout << "in_horas -> " << in_horas << "\t";
-
-    if ( in_horas <= iHorasTaxaMinima ) 
-        dResposta = dTaxaMinima;
-    else {
-        double dExcedido (in_horas - iHorasTaxaMinima);
-        cout <<  "dExcedido -> " << dExcedido << "\t";
-        dResposta = dTaxaMinima + (dExcedido*0.5);
+    if ( in_horas < 24 ) {
+        if ( in_horas <= iHorasTaxaMinima ) 
+            dResposta = dTaxaMinima;
+        else {
+            double dExcedido (in_horas - iHorasTaxaMinima);
+            dResposta = dTaxaMinima + (dExcedido*0.5);
+        }
     }
+    else
+       dResposta = 10 ;
 
-    cout << "dResposta -> " << dResposta ;
-
-    cout << endl ;
     return dResposta;
 }
 
