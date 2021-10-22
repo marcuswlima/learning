06d470e42e064b369434045f262da6633454bcd3
marcus_umarizal <marcuswlima@gmail.com>
2021-10-22 19:37:16 -0300

    exe06_26

--- c++/Deitel/src/cap06/exe06_26.cpp
+++ c++/Deitel/src/cap06/exe06_26.cpp
@@ -12,54 +12,117 @@ using std::endl;
 using std::setw; // permite que o programa configure a largura de um campo
 using std::setprecision;
 
-int countSecondFromZero(int hora, int minuto, int segundo);
-int diferencaEmSegundos(int h1, int m1, int s1, int h2, int m2, int s2);
+class Tempo{
+public:
+    void setHora(int);
+    void setMinuto(int);
+    void setSegundo(int);
+    int getHora();
+    int getMinuto();
+    int getSegundo();
+    void setTempo(int, int, int);
+    void apresentar();
+private:
+    int hora;
+    int minuto;
+    int segundo;
+};
+
+void Tempo::setHora(int x){
+    hora = x;
+}
+
+void Tempo::setMinuto(int x){
+    minuto = x;
+}
+
+void Tempo::setSegundo(int x){
+    segundo = x;
+}
+
+void Tempo::setTempo(int h, int m, int s){
+    setHora(h);
+    setMinuto(m);
+    setSegundo(s);
+}
+
+void Tempo::apresentar(){
+    cout << setw(2) << getHora()    << ":" 
+         << setw(2) << getMinuto()  << ":" 
+         << setw(2) << getSegundo() ;
+}
+
+int Tempo::getHora(){
+    return hora;
+}
+
+int Tempo::getMinuto(){
+    return minuto;
+}
+
+int Tempo::getSegundo(){
+    return segundo;
+}
+
+int countSecondFromZero2(Tempo h1);
+int diferencaEmSegundos2(Tempo h1, Tempo h2);
+
 
 int main(){
 
     srand( time(0) );
 
-    int hora1, minuto1, segundo1,
-        hora2, minuto2, segundo2,
-        cfz1, cfz2;
+    Tempo h1, h2;
 
-    //cout << "Informe a quatidade de vezes :" << endl;
-    //cin >> quantidade ;
+    cout << "Hora1\t\tcsfz1\tHora2\t\tcsfz2\tDiff" << endl;
+    cout << "--------\t-----\t-----\t\t-----\t----" << endl;
 
-    cout << "Hora1\t\tcsfz1\tHora2\tcsfz1\tDiff" << endl;
-    
-    for (int i=1; i<=30; i++){
+    for (int i=1; i<=500; i++){
 
-        hora1 = gerarInteiro(0,11);
-        minuto1 = gerarInteiro(0,59);
-        segundo1 = gerarInteiro(0,59);
-        cfz1 = countSecondFromZero(hora1,minuto1,segundo1);
+        h1.setTempo(gerarInteiro(0,11)
+                   ,gerarInteiro(0,59)
+                   ,gerarInteiro(0,59)
+                   );
 
-        hora2 = gerarInteiro(0,11);
-        minuto2 = gerarInteiro(0,59);
-        segundo2 = gerarInteiro(0,59);
-        cfz2 = countSecondFromZero(hora2,minuto2,segundo2);
+        h2.setTempo(gerarInteiro(0,11)
+                   ,gerarInteiro(0,59)
+                   ,gerarInteiro(0,59)
+                   );
 
-        cout << setw(2) << hora1    << ":" 
-             << setw(2) << minuto1  << ":" 
-             << setw(2) << segundo1 << "\t" 
-             << setw(6) << cfz1     << " " 
-             << setw(2) << hora2    << ":" 
-             << setw(2) << minuto2  << ":" 
-             << setw(2) << segundo2 << "\t"
-             << setw(6) << cfz2     << "\t"
-             << setw(6) << diferencaEmSegundos(hora1, minuto1, segundo1, hora2, minuto2, segundo2) 
-             << endl ;
-             //<< setw(5) << countSecondFromZero(hora,minuto,segundo) << endl;
+        h1.apresentar();
+        cout << "\t" << countSecondFromZero2(h1);
+        cout << "\t" ;
+        h2.apresentar();
+        cout << "\t" << countSecondFromZero2(h2);
+        cout << "\t" << diferencaEmSegundos2(h1,h2);
+        cout << endl;
     }
+    cout << endl;
 
+
+
+    h1.setTempo(0,1,0);
+    h2.setTempo(11,59,59);
+
+    h1.apresentar();
+    cout << "\t" << countSecondFromZero2(h1);
+    cout << "\t" ;
+    h2.apresentar();
+    cout << "\t" << countSecondFromZero2(h2);
+    cout << "\t" << diferencaEmSegundos2(h1,h2);
+    cout << endl;
 
 }
 
-int countSecondFromZero(int hora, int minuto, int segundo){
-    return (hora*60*60) + (minuto*60) + segundo;
+int countSecondFromZero2 (Tempo h1){
+    return (h1.getHora()*60*60) + 
+           (h1.getMinuto()*60)  + 
+            h1.getSegundo();
 }
 
-int diferencaEmSegundos(int h1, int m1, int s1, int h2, int m2, int s2){
-    return abs(countSecondFromZero(h1, m1, s1) - countSecondFromZero(h2, m2, s2));
+int diferencaEmSegundos2(Tempo h1, Tempo h2) {
+    return abs(countSecondFromZero2(h1)-
+               countSecondFromZero2(h2)
+              );
 }
+
