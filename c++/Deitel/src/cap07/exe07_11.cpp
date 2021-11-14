0b731e9457d5c090c26036eaaf7a3698db846b3d
marcus_umarizal <marcuswlima@gmail.com>
2021-11-14 19:24:31 -0300

    exe07_11

--- c++/Deitel/src/cap07/exe07_11.cpp
+++ c++/Deitel/src/cap07/exe07_11.cpp
@@ -1,24 +1,35 @@
 #include "../lib/marcusLib.h"
 
+#include <iostream>
+using std::cout;
+
 int main(){
-    const int size=6;
-    int arrei[size], temp;
+    srand( time(0) );
+
+    const int size=10;
+    int arrei[size]={4,6,3}, temp;
 
     for (int i=0;i<size; i++)
-        arrei[i] = gerarInteiro(1,10);
+        arrei[i] = gerarInteiro(1,100);
     
     mostarArray(arrei,size);
 
-    for (int i=0;i<size; i++){
+    for (int e=0; e<size; e++){
 
-        int e=9;
-        if (arrei[i] > arrei[i+1]){
-            temp = arrei[i+1];
-            arrei[i] = arrei[i+1];
-            arrei[i+1] = temp;
+        for (int i=1;i<size; i++){
+            if ( arrei[i-1] > arrei[i] ){
+                temp = arrei[i];
+                arrei[i] = arrei[i-1];
+                arrei[i-1] = temp;
+            }
         }
 
+        mostarArray(arrei,size);
     }
 
-    mostarArray(arrei,size);
+
+//    mostarArray(arrei,size);
+
+    return 0;
+
 }
