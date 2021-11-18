6794210b8a54c0c97921142014b5e3e9670cbb8a
marcus_umarizal <marcuswlima@gmail.com>
2021-11-18 01:03:53 -0300

    exe07_24

--- c++/Deitel/src/cap07/exe07_24/Board.h
+++ c++/Deitel/src/cap07/exe07_24/Board.h
@@ -1,17 +1,29 @@
+class Celula{
+public:
+    void setBit(int);
+     int getBit();
+    void setOrdem(int);
+     int getOrdem();
+private:
+    int bit;
+    int ordem;
+};
+
 class Board{
 public:
-    Board();
+         Board();
     void setCurrentRow(int);
-    int getCurrentRow();
+     int getCurrentRow();
     void setCurrentColumn(int);
-    int getCurrentColumn();
+     int getCurrentColumn();
     void movimentar();
     void setInicio(int,int);
 private:
-    static const int colunas=8;
-    int board[colunas][colunas][2];
-    int currentRow;
-    int currentColumn;
-    int horizontal[colunas];
-    int vertical[colunas];
+  static const int colunas=8;
+     int board[colunas][colunas][2];
+  Celula board2[colunas][colunas];
+     int currentRow;
+     int currentColumn;
+     int horizontal[colunas];
+     int vertical[colunas];
 };
