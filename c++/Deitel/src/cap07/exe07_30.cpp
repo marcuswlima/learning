1976653b8d73831d831154cac9fc30d5085bcf95
Marcus William <marcus.william@tjpa.jus.br>
2021-11-23 17:04:26 -0300

    exe07_30

--- c++/Deitel/src/cap07/exe07_30.cpp
+++ c++/Deitel/src/cap07/exe07_30.cpp
@@ -4,7 +4,7 @@
 using std::cout;
 
 const int arraySize=3;
-int unidimensional[arraySize]={97,3,1086};
+int unidimensional[arraySize]={97,3,100};
 int buckets[10][arraySize]={0};
 
 int unidade(int);
@@ -44,34 +44,26 @@ void bucketSort(const int arrei[], const int arraySize){
     for (int subscrito=0 ; subscrito<arraySize ; subscrito++){
         buckets[unidade(arrei[subscrito])][0] = arrei[subscrito];
     }
-
+/*
     for (int subscrito=0 ; subscrito<10 ; subscrito++){
         if (buckets[subscrito]!=0);
 
     }
-
+*/
 }
 
 int unidade(int valor){
-
     return valor % 10;
-
 }
 
 int dezena(int valor){
-
     return unidade(valor/10);
-
 }
 
 int centena(int valor){
-
     return unidade(valor/100);
-
 }
 
 int milhar(int valor){
-
     return unidade(valor/1000);
-
 }
--- c++/Deitel/src/cap07/wcompilar.bat
+++ c++/Deitel/src/cap07/wcompilar.bat
@@ -1,22 +1,24 @@
 echo off
-set dados=D:\Users\marcus.william\Developer\dados
+set dados=D:\Users\marcus.william\Developer\_dados\c++
 set BIN=%dados%\%1.exe
 set OBJ=%dados%\%1.o
 set ML=%dados%\marcusLib.o
 
-echo Deletando...
+echo Deleting...
 del %BIN%
 del %OBJ%
 del %ML%
 
-echo Compiling and Liking...
+echo Compiling...
 
 g++ -c ../lib/marcusLib.cpp -o %ML%
 g++ -c %1.cpp               -o %OBJ%
 
+echo Linking...
+
 g++ -Wextra -Werror %ML% %OBJ% -o %BIN%
 
-echo Executando...
+echo Executing...
 echo -------------------------
 
 %BIN%
