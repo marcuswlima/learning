d79215f0c3ca5c56a95f139f82a2948ec7ce7ae9
Marcus William <marcus.william@tjpa.jus.br>
2021-11-25 16:44:37 -0300

    exe07_30

--- c++/Deitel/src/cap07/exe07_30.cpp
+++ c++/Deitel/src/cap07/exe07_30.cpp
@@ -5,34 +5,43 @@ using std::cout;
 
 #include <iomanip>
 
-#define TAM 40000 /*Tamanho do vetor*/
+#define TAM 37000 /*Tamanho do vetor*/
 
 int bucketSort( int [], const int );
 int bubbleSort( int [], const int );
 int insertionSort( int [], const int );
+int selectionSort( int [], const int );
 
 int main(){
 
     srand( time(0) );
 
 
-    int arrInsertionSort[TAM], arrBucketSort[TAM], arrBubbleSort[TAM], tempo;
+    int arrSelectionSort[TAM], arrInsertionSort[TAM], arrBucketSort[TAM], arrBubbleSort[TAM], tempo;
     
     for (int subscrito=0 ; subscrito<TAM ; subscrito++){
         int numero = gerarInteiro(1,999);
         arrBucketSort[subscrito] = numero;
         arrBubbleSort[subscrito] = numero;
         arrInsertionSort[subscrito] = numero;
+        arrSelectionSort[subscrito] = numero;
     }
 
+    cout << "bucketSorting...\n";
     tempo = bucketSort(arrBucketSort,TAM);
-    cout << "bucketSort - Tempo de execucao " << tempo/60 << " Minutos e " << tempo%60 << " segundos.\n";
+    cout << "Tempo de execucao " << tempo/60 << " Minutos e " << tempo%60 << " segundos.\n";
 
+    cout << "bubbleSorting...\n";
     tempo = bubbleSort(arrBubbleSort,TAM);
-    cout << "bubbleSort - Tempo de execucao " << tempo/60 << " Minutos e " << tempo%60 << " segundos.\n";
+    cout << "Tempo de execucao " << tempo/60 << " Minutos e " << tempo%60 << " segundos.\n";
 
+    cout << "insertSorting...\n";
     tempo = insertionSort(arrInsertionSort,TAM);
-    cout << "insertionSort - Tempo de execucao " << tempo/60 << " Minutos e " << tempo%60 << " segundos.\n";
+    cout << "Tempo de execucao " << tempo/60 << " Minutos e " << tempo%60 << " segundos.\n";
+
+    cout << "selectionSorting...\n";
+    tempo = selectionSort(arrSelectionSort,TAM);
+    cout << "Tempo de execucao " << tempo/60 << " Minutos e " << tempo%60 << " segundos.\n";
 }    
 
 int unidade(int valor){
@@ -108,21 +117,28 @@ int bucketSort( int arrei[], const int arraySize){
 
 int bubbleSort( int arrei[], const int arraySize){
 
-    int tinicio, tfim, temp;
+    int tinicio, tfim, temp, desoedenados=arraySize;
+    bool trocou;
 
     tinicio=time(NULL);
 
-
     for (int e=0; e<arraySize; e++){
 
-        for (int i=1;i<arraySize; i++){
+        trocou=false;
+
+        for (int i=1; i<desoedenados; i++){
             if ( arrei[i-1] > arrei[i] ){
-                temp = arrei[i];
+                int temp = arrei[i];
                 arrei[i] = arrei[i-1];
                 arrei[i-1] = temp;
+                trocou = true;
             }
         }
+        desoedenados--;
 
+        if (!trocou)
+            break;
+        
     }
 
 
@@ -140,22 +156,45 @@ int insertionSort( int arrei[], const int arraySize){
  
     for ( int next = 1; next < arraySize; next++ )
     {
-        insert = arrei[ next ]; // armazena o valor no elemento atual
+        insert = arrei[ next ]; 
 
-        int moveItem = next; // inicializa a localização para colocar elemento
+        int moveItem = next; 
 
-        // procura a localização em que colocar o elemento atual
         while ( ( moveItem > 0 ) && ( arrei[ moveItem - 1 ] > insert ) )
         {
-            // desloca o elemento uma posição para a direita
             arrei[ moveItem ] = arrei[ moveItem - 1 ];
             moveItem--;
-        } // fim do while
-        arrei[ moveItem ] = insert; // lugar em que o elemento é inserido no array
-    } // fim do for
+        } 
+        arrei[ moveItem ] = insert; 
+    } 
 
 
     tfim=time(NULL);
 
     return difftime(tfim,tinicio);
+}
+
+
+int selectionSort(int vetor[], const int tam) {
+
+    int tinicio, tfim;
+
+    tinicio=time(NULL);
+
+    for (int indice = 0; indice < tam; ++indice) {
+        int indiceMenor = indice;
+        for (int indiceSeguinte = indice+1; indiceSeguinte < tam; ++indiceSeguinte) {
+            if (vetor[indiceSeguinte] < vetor[indiceMenor]) {
+                indiceMenor = indiceSeguinte;
+            }
+        }
+        int aux = vetor[indice];
+        vetor[indice] = vetor[indiceMenor];
+        vetor[indiceMenor] = aux;
+    }
+
+    tfim=time(NULL);
+
+    return difftime(tfim,tinicio);
+
 }
