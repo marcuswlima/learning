312d114b11304b4e8d846300b6a26b696b5f3743
marcus_umarizal <marcuswlima@gmail.com>
2021-11-25 23:41:01 -0300

    exe07_31

--- c++/Deitel/src/cap07/exe07_31.cpp
+++ c++/Deitel/src/cap07/exe07_31.cpp
@@ -5,17 +5,17 @@ using std::cout;
 
 #include <iomanip>
 
-void selectionSort(int arrei[], const int tamanho);
+void selectionSort(int arrei[], const int tamanho, const int inicio=0);
 
 int main(){
 
-    //srand( time(0) );
+    srand( time(0) );
 
-    const int tamanhoArrei=3;
-    int numeros[tamanhoArrei]={3,2,1};
+    const int tamanhoArrei=30;
+    int numeros[tamanhoArrei]={0};
 
-//    for (int subscrito=0; subscrito<tamanhoArrei; subscrito++)
-//        numeros[subscrito]=gerarInteiro(1,1000);
+    for (int subscrito=0; subscrito<tamanhoArrei; subscrito++)
+        numeros[subscrito]=gerarInteiro(1,100);
 
     mostarArray(numeros,tamanhoArrei);
     selectionSort(numeros,tamanhoArrei);
@@ -23,51 +23,34 @@ int main(){
 
 }
 
-void selectionSort(int arrei[], const int tamanho){
-
-    cout << "selectionSorting...\n";
-
-    int maior=0, subscrito;
-
-    for(int subscrito=0; subscrito<tamanho; subscrito++)
-        if (arrei[subscrito]>maior)
-            maior=arrei[subscrito];
-    
-    int menor=maior;
-    for(int subscrito=0; subscrito<tamanho; subscrito++)
-        if (arrei[subscrito]<menor)
-            menor=arrei[subscrito];
-
-    cout << "maior -> " << maior << "\n";
-    cout << "menor -> " << menor << "\n";
-//-------------------------------------//
+void selectionSort(int arrei[], const int tamanho, const int inicio){
 
     
-    int inicio, subscritoMenor,analisado, temp;
-
-    inicio = tamanho-tamanho;
+    int subscritoMenor,analisado, temp;
+    bool fez=false;
     analisado=arrei[inicio];
 
-    for (int i=1; i<tamanho; i++){
-        if (arrei[i]<analisado){
-            subscritoMenor = i;
+    if (inicio < tamanho){
+
+        for (int i=inicio+1; i<tamanho; i++){
+            if (arrei[i]<analisado){
+                fez=true;
+                subscritoMenor = i;
+                analisado = arrei[i];
+            }
         }
-    }
 
-    temp=arrei[inicio];
-    arrei[inicio]=arrei[subscritoMenor];
-    arrei[subscritoMenor]=temp;
-
-    inicio = 1;
-    analisado=arrei[inicio];
-
-    for (int i=1; i<tamanho; i++){
-        if (arrei[i]<analisado){
-            subscritoMenor = i;
+        if (fez){ 
+            temp=arrei[inicio];
+            arrei[inicio]=arrei[subscritoMenor];
+            arrei[subscritoMenor]=temp;
         }
-    }
 
-    temp=arrei[inicio];
-    arrei[inicio]=arrei[subscritoMenor];
-    arrei[subscritoMenor]=temp;
+        selectionSort(arrei, tamanho,inicio+1);
+
+    }
+    
+
+
+
 }
