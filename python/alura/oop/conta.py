a047ce548cbe93cc1c7f946d74526b195e3535b7
marcus_umarizal <marcuswlima@gmail.com>
2022-01-08 16:31:00 -0300

    py - Restruraçao do projeto na IDE pycharm

---
+++ python/alura/oop/conta.py
@@ -0,0 +1,21 @@
+
+class Conta:
+    def __init__(self,numero, titular, saldo, limite  = 1000.0):
+        print("Construindo objeto....{}".format(self))
+        self.__nomero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} é  {}".format(self.__titular ,self.__saldo ))
+
+    def deposita(self,valor):
+        self.__saldo += valor
+
+    def saca(self,valor):
+        self.__saldo -= valor
+
+    def transfere(self,valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
