eb214a0f35b80330bec40dd805338b789ac8fe1e
Marcus William <marcuswlima@gmail.com>
2022-01-11 13:26:51 -0300

    py - Python 3: Avançando na orientação a objetos - Quando não usar herança

--- python/alura/oo2/modelo.py
+++ python/alura/oo2/modelo.py
@@ -15,26 +15,82 @@ class Programa:
     def nome(self):
         return self._nome
 
+    def imprime(self):
+        print(f'Nome: {self._nome} - Ano: {self.ano} - {self._likes} likes')
+
+    def __str__(self):
+        return f'{self._nome} / {self.ano} / {self._likes} Likes'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome,ano)
         self.duracao = duracao
+
+
+    def imprime(self):
+        print(f'Nome: {self._nome} - Duracao: {self.duracao} mim - Ano: {self.ano} - {self._likes} like(s)')
+
+    def __str__(self):
+        return f'{self._nome} / {self.duracao} mim / {self.ano} / {self._likes} Likes'
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def imprime(self):
+        print(f'Nome: {self._nome} - Temporadas: {self.temporadas} - Ano: {self.ano} - {self._likes} like(s)')
+
+
+    def __str__(self):
+        return f'{self._nome} / {self.temporadas} temporadas / {self.ano} / {self._likes} Likes'
+
+
+class Playlist:
+    def __init__(self,nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
 
 vingadores = Filme('Vingadores - guerra infinita',2018,160)
+atlanta = Serie('atlanta',2018,2)
+tmep = Filme('todo mundo em panico',1999,100)
+demolidor = Serie('Demolidor',2016,2)
+opc = Filme('o poderoso chefao',1979,200)
+twd = Serie('The walkind dead',2010,10)
 
 vingadores.dar_like()
-vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - duracao: {vingadores.duracao} - likes {vingadores.likes}')
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+opc.dar_like()
+opc.dar_like()
+twd.dar_like()
+twd.dar_like()
+twd.dar_like()
 
-atlanta = Serie('atlanta',2018,4)
-atlanta.dar_like()
-atlanta.dar_like()
-atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
-print('Nome: {} / Ano: {} / Temporadas: {} / Likes {}'.format(atlanta.nome,atlanta.ano,atlanta.temporadas, atlanta.likes))
+filmes_e_series = [vingadores, twd, atlanta, demolidor, tmep, twd]
+
+play_list_fim_de_semana = Playlist('fim de semana',filmes_e_series)
+
+print (f'O tamanho da play_list: {len(play_list_fim_de_semana._programas)}')
+
+for programa in play_list_fim_de_semana.listagem:
+    print(programa)
+
+print (f'Tá ou não tá {twd in play_list_fim_de_semana.listagem}')
