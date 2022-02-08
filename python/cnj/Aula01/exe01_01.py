c7672f56540e9d91b58700c52477591aa5f258e8
Marcus William <marcuswlima@gmail.com>
2022-02-08 17:05:09 -0300

    CNJ Python Commit via Pycharm

--- python/cnj/Aula01/exe01_01.py
+++ python/cnj/Aula01/exe01_01.py
@@ -1,19 +1,17 @@
-def virgura_to_ponto(com_virgula):
+def virgula_to_ponto(com_virgula):
     return com_virgula.replace(',','.')
 
 
-print("***********************************")
-print(" IMC=peso/(altura*altura)")
-print("***********************************")
+print("*********************")
+print("** Cálculo do  IMC **")
+print("*********************")
 
-print("peso")
-print("altura")
 
 altura_informada = input("Informe a altura: ")
 peso_informado = input("Informe o peso: ")
 
-altura = float(virgura_to_ponto(altura_informada))
-peso = float(virgura_to_ponto(peso_informado))
+altura = float(virgula_to_ponto(altura_informada))
+peso = float(virgula_to_ponto(peso_informado))
 imc = peso / (altura*altura)
 
 print("altura -> ", altura)
