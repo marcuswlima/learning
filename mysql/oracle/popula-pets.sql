9f9ff7c244632c32931e078ee807db5ab111478e
Marcus William <marcus.william@tjpa.jus.br>
2022-02-12 19:13:58 -0300

    [mysql] criação banco

---
+++ mysql/oracle/popula-pets.sql
@@ -0,0 +1,12 @@
+delete from pet;
+       
+INSERT INTO pet values('Fluffy','Harold','cat','f','1993-02-04',null);
+INSERT INTO pet values('Claws','Gwen','cat','m','1994-03-17',null);	
+INSERT INTO pet values('Buffy','Harold','dog','f','1989-05-13',null);		
+INSERT INTO pet values('Fang','Benny','dog','m','1990-08-27',null);		
+INSERT INTO pet values('Bowser','Diane','dog','m','1979-08-31','1995-07-29');	
+INSERT INTO pet values('Chirpy','Gwen','bird','f','1998-09-11',null);	
+INSERT INTO pet values('Whistler','Gwen','bird',null,'1997-12-09',null);	
+INSERT INTO pet values('Slim','Benny','snake','m','1996-04-29',null);	
+
+
