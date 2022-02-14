47ff46f1ffb4cb52491faa7b9173dea6294670dc
Marcus William <marcuswlima@gmail.com>
2022-02-14 08:39:12 -0300

    [CNJ/Python] Ajustes código

--- python/cnj/Aula02/ingressos.py
+++ python/cnj/Aula02/ingressos.py
@@ -1,39 +1,40 @@
-
+'''
 def eh_numero_natural(digitado):
-    resposta=True
+    resposta = True
     try:
-        nnatural=int(digitado)
+        nnatural = int(digitado)
         resposta = (nnatural >= 0)
     except:
-        resposta=False
+        resposta = False
     return resposta
 
+# print(eh_numero_natural(digitado))
 
+'''
 
-valor_base=10
-#desconto=0
-idade=0
+valor_base = 10
+# desconto=0
+idade = 0
 
 while True:
 
     digitado = input("Informe uma idade ou digite 'q' para sair: ")
-    print(eh_numero_natural(digitado))
 
     digitado = digitado.upper()
-    if (digitado=="Q"):
+    if digitado == "Q":
         break
 
     idade = int(digitado)
-    if (idade<5):
-        desconto=100
-    elif (idade>=5) and (idade<=12):
-        desconto=50
-    elif (idade>=13) and (idade<=17):
-        desconto=20
-    elif (idade>=18) and (idade<=70):
-        desconto=0
-    elif (idade>=70):
-        desconto=100
+    if idade < 5:
+        desconto = 100
+    elif (idade >= 5) and (idade <= 12):
+        desconto = 50
+    elif (idade >= 13) and (idade <= 17):
+        desconto = 20
+    elif (idade >= 18) and (idade < 70):
+        desconto = 0
+    else:  # idade>=70
+        desconto = 100
 
     print("{}% de desconto".format(desconto))
-    print("O valor a ser pago é {}".format(valor_base - (valor_base*desconto/100)))
+    print("O valor a ser pago é {}".format(valor_base - (valor_base * desconto / 100)))
