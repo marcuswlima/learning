e083a445ebb306f83d3f73fffdb3df8c949fc532
Marcus William <marcuswlima@gmail.com>
2022-02-22 12:54:20 -0300

    ee02_01

--- python/cnj/aula02/ee02_01.py
+++ python/cnj/aula02/ee02_01.py
@@ -1,7 +1,7 @@
 import utilitarios
 
-def executar_01_01():
-    utilitarios.titulo("Cálculo do IMC")
+def ee02_01():
+    utilitarios.titulo("Faixa do IMC")
 
     altura = utilitarios.obter_float("Informe a altura: ")
     peso = utilitarios.obter_float("Informe o peso: ")
@@ -18,8 +18,8 @@ def executar_01_01():
     elif imc>=18.5 and imc<=24.9:
         faixa="Nornal"
 
-    print("Com o IMC={} de desconto".format(desconto))
+    print("Com o IMC={} a sua faixa e {}".format(imc,faixa))
     
 
 if __name__ == "__main__":
-    executar_01_01()
+    ee02_01()
