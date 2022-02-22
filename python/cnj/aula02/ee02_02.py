8046b95745f8cc6daa7be6be7025f85a3eb5d89e
Marcus William <marcus.william@tjpa.jus.br>
2022-02-22 17:06:06 -0300

    [CNJ / Python ]
    Produção dos exercícios extra da aula2

---
+++ python/cnj/aula02/ee02_02.py
@@ -0,0 +1,31 @@
+import utilitarios
+
+def ee02_02():
+    utilitarios.titulo("Faixa do IMC")
+
+    altura = utilitarios.obter_float("Informe a altura: ")
+    peso = utilitarios.obter_float("Informe o peso: ")
+    imc = utilitarios.imc(peso,altura)
+
+    print("altura -> ", altura)
+    print("peso -> ", peso)
+    print("imc -> ", imc)
+
+    faixa="Normal"
+
+    if (imc<18.5):
+        faixa="Magreza"
+    elif imc>=18.5 and imc<30.0:
+        faixa="Nornal"
+    elif imc>=25 and imc<30:
+        faixa="Sobrepeso"
+    elif imc>=30 and imc<40:
+        faixa="Obesidade"
+    elif imc>=40:
+        faixa="Obesidade Grace"
+
+    print("Com o IMC={} a sua faixa e {}".format(imc,faixa))
+    
+
+if __name__ == "__main__":
+    ee02_02()
---
+++ python/cnj/aula02/ee02_03.py
@@ -0,0 +1,21 @@
+import utilitarios
+
+def ee02_03():
+    utilitarios.titulo("exercicio extra 02.03")
+
+    cidade_informada = "inicio"
+    maior_populacao=0
+    cidade_mais_populosa="inicio"
+    while (cidade_informada!="sair"):
+        cidade_informada = input("Informe o nome da cidade: ")
+        if(cidade_informada!="sair"):
+            populacao_da_cidade = input("Informe a polulação: ")
+            if (int(populacao_da_cidade) > int(maior_populacao)):
+                maior_populacao = populacao_da_cidade
+                cidade_mais_populosa = cidade_informada
+
+                
+    print("A cidade com maior população é {} com seus {} habitantes".format(cidade_mais_populosa,maior_populacao))
+
+if __name__ == "__main__":
+    ee02_03()
