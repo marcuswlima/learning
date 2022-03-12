b2fde88e682f5b19b8a1d3d612d89e8caea12e69
Marcus William <marcuswlima@gmail.com>
2022-03-12 14:26:52 -0300

    [Deiltel/c++]
    Conclusão 8.7 sizeof operator
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   c++/Deitel/src/cap08/fig08_13.cpp

--- c++/Deitel/src/cap08/fig08_13.cpp
+++ c++/Deitel/src/cap08/fig08_13.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 void selectionSort( int * const, const int ); // prototype
 void swap( int * const, int * const ); // prototype
+void selectionSort2( int [], const int ); // prototype
 
 int main()
 {
@@ -18,7 +19,7 @@ int main()
     for ( int i = 0; i < arraySize; ++i )
         cout << setw( 4 ) << a[ i ];
 
-    selectionSort( a, arraySize ); // sort the array
+    selectionSort2( a, arraySize ); // sort the array
 
     cout << "\nData items in ascending order\n";
 
@@ -56,3 +57,25 @@ void swap( int * const element1Ptr, int * const element2Ptr ) // (Const pointer 
     *element1Ptr = *element2Ptr;
     *element2Ptr = hold;
 } // end function swap
+
+
+void selectionSort2( int array[], const int size) // prototype
+{
+
+    int smallest; // index of smallest element
+
+    // loop over size - 1 elements
+    for ( int i = 0; i < size - 1; ++i )
+    {
+        smallest = i; // first index of remaining array
+
+        // loop to find index of smallest element
+        for ( int index = i + 1; index < size; ++index )
+
+            if ( array[ index ] < array[ smallest ] )
+                smallest = index;
+
+        swap( &array[ i ], &array[ smallest ] );
+    } // end if
+
+}
