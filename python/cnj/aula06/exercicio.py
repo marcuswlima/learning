841c7355930af7846fea37db185cb3167127869f
Marcus William <marcuswlima@gmail.com>
2022-03-29 12:34:19 -0300

    [ cnj / python ]
    Prover tratamento de exeção à leitura de arquivo
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   aula06/exercicio.py

--- python/cnj/aula06/exercicio.py
+++ python/cnj/aula06/exercicio.py
@@ -5,8 +5,10 @@ import numpy       as np
 #----------------------------
 utl.titulo("Exercicio 06")
 
-
-data = np.genfromtxt("../../../../../dados/python/Dados.csv", delimiter=';')
+try:
+    data = np.genfromtxt("../../../../../dados/python/Dados.csv", delimiter=';')
+except:
+    data = np.genfromtxt("..\..\..\..\..\dados\python\Dados.csv", delimiter=';')
 
 ocorrencias = data.shape[0]
 qtd_colunas = 15
@@ -21,3 +23,5 @@ for linha in new:
 
 print(new.sum(axis=1))
 print(new.sum())
+
+
