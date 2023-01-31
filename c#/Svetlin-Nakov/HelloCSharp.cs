19f9d3b57609a5b2ee1e1bba3b3885ea4a3fbf79
Marcus William <marcuswlima@gmail.com>
2023-01-31 12:58:08 -0300

    Svetlin-Nakov
    Projeto C# com posicionamento de solução
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .gitignore
    	new file:   c#/Svetlin-Nakov/HelloCSharp.cs
    	new file:   c#/Svetlin-Nakov/IntroToCSharp/IntroToCSharp.sln
    	new file:   c#/Svetlin-Nakov/IntroToCSharp/IntroToCSharp/IntroToCSharp.csproj
    	new file:   c#/Svetlin-Nakov/IntroToCSharp/IntroToCSharp/Program.cs
    	renamed:    c#/alura/AprendendoCsharp/1-olamundo/1-olamundo.csproj -> c#/alura/AprendendoCsharp/1-olamundo/P01-olamundo.csproj
    	renamed:    c#/alura/AprendendoCsharp/2-criandovariaveis/2-criandovariaveis.csproj -> c#/alura/AprendendoCsharp/2-criandovariaveis/P02-criandovariaveis.csproj
    	renamed:    c#/alura/AprendendoCsharp/3-CriandoVariaveisPontoFlutuante/3-CriandoVariaveisPontoFlutuante.csproj -> c#/alura/AprendendoCsharp/3-CriandoVariaveisPontoFlutuante/P03-CriandoVariaveisPontoFlutuante.csproj
    	renamed:    c#/alura/AprendendoCsharp/4-Conversoes/4-Conversoes.csproj -> c#/alura/AprendendoCsharp/4-Conversoes/P04-Conversoes.csproj
    	renamed:    c#/alura/AprendendoCsharp/5-CaracteresETextos/5-CaracteresETextos.csproj -> c#/alura/AprendendoCsharp/5-CaracteresETextos/P05-CaracteresETextos.csproj
    	renamed:    c#/alura/AprendendoCsharp/7-Condicionais/7-Condicionais.csproj -> c#/alura/AprendendoCsharp/7-Condicionais/P07-Condicionais.csproj
    	renamed:    c#/alura/AprendendoCsharp/8-Condicionais-2/8-Condicionais-2.csproj -> c#/alura/AprendendoCsharp/8-Condicionais-2/P08-Condicionais-2.csproj
    	renamed:    c#/alura/AprendendoCsharp/LacoDeRepeticaoFor/LacoDeRepeticaoFor.csproj -> c#/alura/AprendendoCsharp/LacoDeRepeticaoFor/P06-LacoDeRepeticaoFor.csproj
    	renamed:    c#/alura/AprendendoCsharp/LacoDeRepeticaoWhile/LacoDeRepeticaoWhile.csproj -> c#/alura/AprendendoCsharp/LacoDeRepeticaoWhile/P09-LacoDeRepeticaoWhile.csproj
    	renamed:    c#/alura/AprendendoCsharp/MultiploDeTres/MultiploDeTres.csproj -> c#/alura/AprendendoCsharp/MultiploDeTres/P14-MultiploDeTres.csproj
    	renamed:    c#/alura/csharp-exceptions/ByteBank/ByteBank.csproj -> c#/alura/csharp-exceptions/ByteBank/ByteBank-exception.csproj
    	renamed:    c#/alura/csharp-oo/bytebank/bytebank.csproj -> c#/alura/csharp-oo/bytebank/bytebank-oo.csproj
    	new file:   c#/cSharpSolution.sln

---
+++ c#/Svetlin-Nakov/HelloCSharp.cs
@@ -0,0 +1,10 @@
+class HelloCSharp
+{
+	static void Main(string[] args)
+	{
+		System.Console.WriteLine("Hello C#!");
+		System.Console.WriteLine("Hello C#!");
+		System.Console.WriteLine("Hello C#!");
+		System.Console.WriteLine("Hello C#!");
+	}
+}
---
+++ c#/Svetlin-Nakov/IntroToCSharp/IntroToCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroToCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
