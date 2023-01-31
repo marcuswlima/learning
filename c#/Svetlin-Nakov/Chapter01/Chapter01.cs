15ed54339f40a9013fc303df7a7914a8dc9538e7
Marcus William <marcuswlima@gmail.com>
2023-01-31 16:05:23 -0300

    Svetlin-Nakov - C# - Chapter 1. Introduction to Programming
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   c#/Svetlin-Nakov/Chapter01/Chapter01.cs
    	renamed:    c#/Svetlin-Nakov/IntroToCSharp/IntroToCSharp/IntroToCSharp.csproj -> c#/Svetlin-Nakov/Chapter01/Chapter01.csproj
    	deleted:    c#/Svetlin-Nakov/HelloCSharp.cs
    	deleted:    c#/Svetlin-Nakov/IntroToCSharp/IntroToCSharp.sln
    	deleted:    c#/Svetlin-Nakov/IntroToCSharp/IntroToCSharp/Program.cs
    	new file:   c#/Svetlin-Nakov/Svetlin-Nakov.sln
    	renamed:    c#/alura/csharp-exceptions/ByteBank/ByteBank-exception.csproj -> c#/alura/csharp-exceptions/ByteBank/P16-ByteBank-exception.csproj
    	renamed:    c#/alura/csharp-oo/bytebank/bytebank-oo.csproj -> c#/alura/csharp-oo/bytebank/P15-ByteBank-oo.csproj
    	modified:   c#/cSharpSolution.sln

---
+++ c#/Svetlin-Nakov/Chapter01/Chapter01.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+
+namespace Chapter01
+{
+    class Chapter01
+    {
+        static void Main()
+        {
+            Titulo("Exercicio 07");
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+
+            Titulo("Exercicio 08");
+            DateTime localDate = DateTime.Now;
+            DateTime utcDate = DateTime.UtcNow;
+            String[] cultureNames = { "en-US", "en-GB", "fr-FR",
+                                "de-DE", "pt-BR", "sp-SP"  };
+
+            foreach (var cultureName in cultureNames)
+            {
+                var culture = new CultureInfo(cultureName);
+                Console.WriteLine("{0}:", culture.NativeName);
+                Console.WriteLine("   Local date and time: {0}, {1:G}",
+                                  localDate.ToString(culture), localDate.Kind);
+                Console.WriteLine("   UTC date and time: {0}, {1:G}\n",
+                                  utcDate.ToString(culture), utcDate.Kind);
+            }
+
+
+            Titulo("Exercicio 09 - square root of 12345.");
+            Console.WriteLine(Math.Sqrt(12345));
+
+            Titulo("Exercicio 10");
+
+            for (int i = 1; i <= 100 ; i++)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            Console.WriteLine(" ");
+
+            Titulo("Exercicio 11");
+
+            Console.Write("Informe a sua idade: ");
+            String informado = Console.ReadLine();
+            int numVal = Int32.Parse(informado)+10;
+            Console.Write("A sua idade após 10 anos será: ");
+            Console.WriteLine(numVal);
+
+            Titulo("Exercicio 12");
+
+        }
+
+        static void ImprimeBarra(String titulo)
+        {
+            int tamanho = titulo.Length;
+
+            for (int i = 1; i <= tamanho + 6; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine(" ");
+        }
+        static void Titulo(String titulo)
+        {
+            Console.WriteLine(" ");
+
+            ImprimeBarra(titulo);
+            Console.WriteLine("** "+titulo+ " **");
+            ImprimeBarra(titulo);
+
+        }
+    }
+}
