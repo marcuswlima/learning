16b404605853cbb533023b1c7404e4559b6a03ea
Marcus William <marcuswlima@gmail.com>
2023-02-01 17:22:02 -0300

    Svetlin-Nakov - c# - Chapter 3. Operators and Expressions - Exercicios
    Exercicio 03
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   c#/Svetlin-Nakov/Chapter03/Program.cs
    	modified:   c#/Svetlin-Nakov/Svetlin-Nakov.sln

--- c#/Svetlin-Nakov/Chapter03/Program.cs
+++ c#/Svetlin-Nakov/Chapter03/Program.cs
@@ -4,15 +4,84 @@ namespace Chapter03
 {
     class Program
     {
+
+        int myArraySize = 100;
+        int[] myArray;
         static void Main()
         {
-            Titulo("Chapter 3. Operators and Expressions");
+            UtilTitulo("Chapter 3. Operators and Expressions");
             Leitura();
+            Exercicio01();
+            Exercicio02();
+            Exercicio03();
+        }
+
+        static int Remainder(int divisor, int dividendo)
+        {
+            return divisor % dividendo;
+        }
+
+        static bool HasRemainder(int divisor, int dividendo)
+        {
+            return Remainder(divisor,dividendo)>0;
+        }
+
+        static bool IsOdd(int numero)
+        {
+
+            return !HasRemainder(numero,2);
+        }
+
+        static bool IsThirdDigitEqual7(int numero)
+        {
+            int semUmaCentena = numero / 100;
+
+            return Remainder(semUmaCentena, 10)==7;
+        }
+
+        static void Exercicio01()
+        {
+            UtilTitulo("Exercicio01");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write(" "+i+"->");
+                if (IsOdd(i))
+                    Console.Write("Par");
+                else
+                    Console.Write("Impar");
+            }
+            Console.WriteLine("");
+
+
+        }
+
+        static void Exercicio02()
+        {
+            UtilTitulo("Exercicio02");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (!HasRemainder(i,5) && !HasRemainder(i,7))
+                    Console.Write(i + " ");
+            }
+            Console.WriteLine("");
+        }
+
+        static void Exercicio03()
+        {
+            UtilTitulo("Exercicio03");
+            for (int i = 100; i <= 10000; i++)
+            {
+                if (IsThirdDigitEqual7(i))
+                    Console.Write(i + " ");
+            }
+            Console.WriteLine("");
         }
 
         static void Leitura()
         {
-            Titulo("Arithmetical Operators");
+            UtilTitulo("Arithmetical Operators");
             {
                 int a, b;
                 a = 5;
@@ -48,7 +117,7 @@ namespace Chapter03
                 Console.WriteLine(" 2134 " + (a + b));
             }
 
-            Titulo("Explicit Type Conversion");
+            UtilTitulo("Explicit Type Conversion");
             {
                 double myDouble = 5.1d;
                 Console.WriteLine(myDouble); // 5.1
@@ -64,7 +133,7 @@ namespace Chapter03
 
         }
 
-        static void ImprimeBarra(String titulo)
+        static void UtilImprimeBarra(String titulo)
         {
             int tamanho = titulo.Length;
 
@@ -76,13 +145,13 @@ namespace Chapter03
             Console.WriteLine(" ");
         }
 
-        static void Titulo(String titulo)
+        static void UtilTitulo(String titulo)
         {
             Console.WriteLine(" ");
 
-            ImprimeBarra(titulo);
+            UtilImprimeBarra(titulo);
             Console.WriteLine("** " + titulo + " **");
-            ImprimeBarra(titulo);
+            UtilImprimeBarra(titulo);
 
         }
     }
