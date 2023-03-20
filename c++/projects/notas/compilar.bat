03e6801482782f5e4906e2a5f7fe865a2c5a8328
Marcus William <marcuswlima@gmail.com>
2023-03-20 08:35:56 -0300

    [c++/notas] Versão para windows
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   c++/projects/libTester/compilar.bat
    	new file:   c++/projects/notas/compilar.bat
    	modified:   c++/projects/notas/src/notas.cpp
    	deleted:    c++/projects/notas/src/notas2.cpp

---
+++ c++/projects/notas/compilar.bat
@@ -0,0 +1,30 @@
+echo off
+set OBJ=obj
+set SRC=src
+set LIB=..\lib
+set NOME=notas
+
+echo BarraTitulo.cpp...
+g++ -c %LIB%\BarraTitulo.cpp    -o %OBJ%\BarraTitulo.o
+echo Mathematics.cpp...
+g++ -c %LIB%\Mathematics.cpp    -o %OBJ%\Mathematics.o
+echo ObterNumero.cpp...
+g++ -c %LIB%\ObterNumero.cpp    -o %OBJ%\ObterNumero.o
+
+IF %ERRORLEVEL% EQU 0 ( 
+    echo %NOME%.cpp...
+    g++ -c %SRC%\%NOME%.cpp -o %OBJ%\%NOME%.o 
+
+    echo Tecla.cpp...
+    g++ -c %SRC%\Tecla.cpp -o %OBJ%\Tecla.o 
+    IF %ERRORLEVEL% EQU 0  (
+
+        echo linkando tudo...
+        g++ -Wextra -Werror %OBJ%\Tecla.o %OBJ%\%NOME%.o %OBJ%\ObterNumero.o %OBJ%\BarraTitulo.o %OBJ%\Mathematics.o -o %OBJ%\%NOME%.exe        
+
+        IF %ERRORLEVEL% EQU 0  (
+            echo Executando...
+            %OBJ%\%NOME%.exe
+        )
+     )
+)
--- c++/projects/notas/src/notas.cpp
+++ c++/projects/notas/src/notas.cpp
@@ -1,135 +1,250 @@
 #include <iostream>
-
-using std::cout;
-using std::cin;
-using std::endl;
+#include <string>
+using namespace std;
 
 #include "Tecla.h"
 #include "../../lib/BarraTitulo.h"
-/////////////////////////////////////////
-// Declarações
-/////////////////////////////////////////
-void imprimir(Tecla);
-Tecla gerar3m(Tecla);
-Tecla gerar3M(Tecla);
-void aumentarAlturaEmUmTom(Tecla &);
-void aumentarAlturaEmMeioTom(Tecla &);
-void gerarTriadeMaior(Tecla , Tecla &, Tecla &);
-void gerarTriadeMenor(Tecla , Tecla &, Tecla &);
-void gerarTriadeDiminuta(Tecla , Tecla &, Tecla &);
-void gerarTriadeAumentada(Tecla , Tecla &, Tecla &);
-void imprimirTriade(Tecla, Tecla, Tecla);
+#include "../../lib/ObterNumero.h"
+#include "../../lib/Mathematics.h"
 
+void IntervalosGerarSegundaNota();
+void IntervalosGerarIntervalo();
+int menu();
+void Notas();
+void Intervalos();
+void Acordes();
+void Acordes_um();
+
+Tecla t;
 /////////////////////////////////////////
 // Main
 /////////////////////////////////////////
 int main ( )
 {
-    srand( time(0) );
+    //srand( time(0) );
 
-    ImprimirTitulo("Notas");
-/*
-    Tecla t1(3,3,-1), t3, t5;
+    int opcao=0;
 
-    gerarTriadeMaior(t1, t3, t5);
-    imprimirTriade(t1, t3, t5);
+    ImprimirTitulo("Nota Musicais");
+    do{
+        opcao=menu();
 
-    gerarTriadeMenor(t1, t3, t5);
-    imprimirTriade(t1, t3, t5);
+		switch(opcao){
+			case 1:Notas();break;
+			case 2:Intervalos();break;
+			case 3:Acordes();break;
+		}
+    }while (opcao!=0);
 
-    gerarTriadeDiminuta(t1, t3, t5);
-    imprimirTriade(t1, t3, t5);
-
-    gerarTriadeAumentada(t1, t3, t5);
-    imprimirTriade(t1, t3, t5);
-*/
     return 0; //indica o fim do programa
 }
 
-void imprimirTriade(Tecla t1, Tecla t3, Tecla t5){
+int menu(){
+	int escolha;
+    cout << "**********************\n";
+    cout << "** 1 - Notas Soltas **\n";
+    cout << "** 2 - Intervalos   **\n";
+    cout << "** 3 - Acordes      **\n";
+    cout << "** 0 - Sair         **\n";
+    cout << "***********************\n";
+	escolha=ObterNumeroNaFaixa("Indique qual módulo [1,2,3,0] -> ",0,3);
+    return escolha;
+}
+
+void Notas(){	
+	cout << endl << endl;
+	ImprimirTitulo("Notas Soltas");
+	int quantNotas=ObterNumeroNaFaixa("Digite a quantidade[1:30] -> ",1,30);
+	cout << endl;
+     
+	for (int i=1;i<=quantNotas;i++){
+		t.aleatorio();
+		t.imprimirTecla();
+	}
+
+	cout << endl << endl;
+}
+
+void Intervalos(){	
+	cout << endl << endl;
+	ImprimirTitulo("Intervalos");
+    cout << "****************************************\n";
+    cout << "** A partir de uma nota aleatória     **\n";
+    cout << "** 1) Apresentar um intervalo simples **\n";
+    cout << "** 2) Apresentar uma segunda nota     **\n";
+    cout << "****************************************\n";
+	int opcao=ObterNumeroNaFaixa("Indica sua opção [1:2] -> ",1,2);
+	int quantidade=ObterNumeroNaFaixa("Informe a quantidade[1:30] -> ",1,30);
+
+	cout << endl;
+	for (int i=1; i<=quantidade; i++){	
+		if (opcao==1)
+			IntervalosGerarIntervalo();
+		else 
+            IntervalosGerarSegundaNota();
+	}
+
+	cout << endl;
+	cout << endl;
+
+}
+
+void IntervalosGerarIntervalo(){
+
+	int numero;
+	t.aleatorio();
+	t.imprimirTecla();
+
+	cout << "- ";
+	numero=GerarInteiro(2,8);
+	cout << numero;
+	if ((numero==4)||(numero==5)||(numero==8))
+		cout << "J";
+	else{
+		numero=GerarInteiro(1,2);
+		if (numero==1) 
+			cout << "m";
+		else 
+			cout << "M";
+	}
+
+    cout << " / ";
+}
+
+int GerarNovaNota(int inferior,int superior,int antiga){	
+	int resposta;
+	do{	
+		resposta = GerarInteiro(inferior,superior);
+	}while(resposta==antiga);
+	return resposta;
+}
+
+void IntervalosGerarSegundaNota(){
+	int notaN1, novaN2;
+	t.aleatorio();
+	t.imprimirTecla();
+
+    notaN1 = t.getNota();
+	novaN2 = GerarNovaNota(1,7,notaN1);
+
+	if(novaN2 < notaN1) 
+		t.setOitava(t.getOitava()+1);
+	
+	t.setNota(novaN2);
+	t.setAcidente(GerarInteiro(-1,1));
+	t.imprimirTecla();
+	
+
+    cout << " / ";
+}
+
+void Acordes(){	
+	cout << endl << endl;
+	ImprimirTitulo("Acordes");
+    cout << "****************************************************\n";
+    cout << "** A partir de uma nota aleatória                 **\n";
+    cout << "** 1) Apresentar um acorde(M,m,A,d)               **\n";
+    cout << "** 2) Apresentar uma terça nota e uma quinta nota **\n";
+    cout << "****************************************************\n";
+	int opcao=ObterNumeroNaFaixa("Indica sua opção [1:2] -> ",1,2);
+
+	if (opcao==1){
+	    int quantidade=ObterNumeroNaFaixa("Informe a quantidade[1:30] -> ",1,30);
+		for(int i=1; i<quantidade; i++){
+			Acordes_um();
+		}
+	}
+	else
+		cout << "Não implementado";
+
+	cout << endl << endl;
+}
+
+void Acordes_um(){
+	int idAcorde=GerarInteiro(1,4);
+	string descAcorde; 
+	t.aleatorio();
+	t.imprimirTecla();
+
+	switch (idAcorde){
+    	case 1:descAcorde="Maior";break;
+    	case 2:descAcorde="Menor";break;
+    	case 3:descAcorde="Aumentado";break;
+    	case 4:descAcorde="Diminuto";break;
+	}
+
+	cout << descAcorde;
+
+	cout << " / ";
+}
+
+/*
+    Tecla t1(3,4,0), t2;
     t1.imprimirTecla();
-    t3.imprimirTecla();
-    t5.imprimirTecla();
-    cout<<"\n";
+    t2 = i2m(t1);
+    t2.imprimirTecla();
+    t2 = i2M(t1);
+    t2.imprimirTecla();
+    t2 = i3m(t1);
+    t2.imprimirTecla();
+    t2 = i3M(t1);
+    t2.imprimirTecla();
+    t2 = i4J(t1);
+    t2.imprimirTecla();
+	cout << endl;
+
+Tecla i2m(Tecla t){
+    Tecla temp;
+    int deslocamentoTecla=1,deslocamentoSemitom=1,distanciaDeNotas=2;
+    copiaTecla(t,temp);
+
+    temp.setNota(temp.getNota()+deslocamentoTecla);
+    temp.setAcidente(deslocamentoSemitom-distanciaDeNotas);
+    return temp;
 }
 
-void gerarTriadeMaior(Tecla t1, Tecla &t3, Tecla &t5){
-    t3=gerar3M(t1);
-    t5=gerar3m(t3);
+Tecla i2M(Tecla t){
+    Tecla temp;
+    int deslocamentoTecla=1,deslocamentoSemitom=2,distanciaDeNotas=2;
+    copiaTecla(t,temp);
+
+    temp.setNota(temp.getNota()+deslocamentoTecla);
+    temp.setAcidente(deslocamentoSemitom-distanciaDeNotas);
+    return temp;
 }
 
-void gerarTriadeMenor(Tecla t1, Tecla &t3, Tecla &t5){
-    t3=gerar3m(t1);
-    t5=gerar3M(t3);
+Tecla i3m(Tecla t){
+    Tecla temp;
+    int deslocamentoTecla=2,deslocamentoSemitom=3,distanciaDeNotas=4;
+    copiaTecla(t,temp);
+
+    temp.setNota(temp.getNota()+deslocamentoTecla);
+    temp.setAcidente(deslocamentoSemitom-distanciaDeNotas);
+    return temp;
 }
 
-void gerarTriadeDiminuta(Tecla t1, Tecla &t3, Tecla &t5){
-    t3=gerar3m(t1);
-    t5=gerar3m(t3);
+Tecla i3M(Tecla t){
+    Tecla temp;
+    int deslocamentoTecla=2,deslocamentoSemitom=4,distanciaDeNotas=4;
+    copiaTecla(t,temp);
+
+    temp.setNota(temp.getNota()+deslocamentoTecla);
+    temp.setAcidente(deslocamentoSemitom-distanciaDeNotas);
+    return temp;
 }
 
-void gerarTriadeAumentada(Tecla t1, Tecla &t3, Tecla &t5){
-    t3=gerar3M(t1);
-    t5=gerar3M(t3);
+Tecla i4J(Tecla t){
+    Tecla temp;
+    int deslocamentoTecla=3,deslocamentoSemitom=5,distanciaDeNotas=6;
+    copiaTecla(t,temp);
+
+    temp.setNota(temp.getNota()+deslocamentoTecla);
+    temp.setAcidente(deslocamentoSemitom-distanciaDeNotas);
+    return temp;
 }
 
-Tecla gerar3m(Tecla t){
-    Tecla r;
-
-    r.setOitava(t.getOitava());
-    r.setNota(t.getNota());
-    r.setAcidente(t.getAcidente());
-
-    aumentarAlturaEmUmTom(r);
-    aumentarAlturaEmMeioTom(r);
-    return r;
+void copiaTecla(Tecla t1,Tecla &t2){
+    t2.setTecla(t1.getOitava(), t1.getNota(), t1.getAcidente());
 }
 
-Tecla gerar3M(Tecla t){
-    Tecla r;
-
-    r.setOitava(t.getOitava());
-    r.setNota(t.getNota());
-    r.setAcidente(t.getAcidente());
-
-    aumentarAlturaEmUmTom(r);
-    aumentarAlturaEmUmTom(r);
-    return r;
-}
-
-void aumentarAlturaEmUmTom(Tecla &t){
-    aumentarAlturaEmMeioTom(t);
-    aumentarAlturaEmMeioTom(t);
-}
-
-// aumentar a tecla em 1/2 tom
-void aumentarAlturaEmMeioTom(Tecla &t){
-
-    if (t.getOitava()!=9){
-
-        if (t.getAcidente()==0) {// teclas bancas 
-            if (t.getNota() == 3)
-                t.setNota(4);
-            else if (t.getNota() == 7 ){
-                t.setNota(1);
-                t.setOitava(t.getOitava()+1);
-            }
-            else
-                t.setAcidente(1);
-
-        }
-        else if (t.getAcidente()==1) {// notas sustenidas
-            t.setNota(t.getNota()+1);
-            t.setAcidente(0);
-        }
-        else if (t.getAcidente()==-1){
-
-            //cout << "*" << getNota() << "-" << getAcidente() <<"*";
-            t.setAcidente(0);
-        }
-    }
-    else
-        cout << "Ultima tecla";
-
-}
-
+*/
