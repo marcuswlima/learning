e385f5e89440138a752e435aeef25857dff6ff55
marcus.william <marcus.william@tjpa.jus.br>
2023-04-05 12:37:58 -0300

    [c++] Implentar desvio de fluxo de execução após erro compilar.bat
    On branch 10-c++-implentar-desvio-de-fluxo-de-execução-após-erro-compilarbat
    Your branch is up to date with 'origin/10-c++-implentar-desvio-de-fluxo-de-execução-após-erro-compilarbat'.
    
    Changes to be committed:
    	modified:   compilar.bat

--- c++/projects/notas/compilar.bat
+++ c++/projects/notas/compilar.bat
@@ -6,32 +6,45 @@ set NOME=notas
 
 echo BarraTitulo.cpp...
 g++ -c %LIB%\BarraTitulo.cpp    -o %OBJ%\BarraTitulo.o
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 echo Mathematics.cpp...
 g++ -c %LIB%\Mathematics.cpp    -o %OBJ%\Mathematics.o
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 echo ObterNumero.cpp...
 g++ -c %LIB%\ObterNumero.cpp    -o %OBJ%\ObterNumero.o
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-IF %ERRORLEVEL% EQU 0 ( 
-    echo %NOME%.cpp...
-    g++ -c %SRC%\%NOME%.cpp -o %OBJ%\%NOME%.o 
 
-    echo Nota.cpp...
-    g++ -c %SRC%\Nota.cpp -o %OBJ%\Nota.o 
+echo Nota.cpp...
+g++ -c %SRC%\Nota.cpp -o %OBJ%\Nota.o 
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-    echo Intervalo.cpp...
-    g++ -c %SRC%\Intervalo.cpp -o %OBJ%\Intervalo.o 
+echo Intervalo.cpp...
+g++ -c %SRC%\Intervalo.cpp -o %OBJ%\Intervalo.o 
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-    echo Triade.cpp...
-    g++ -c %SRC%\Triade.cpp -o %OBJ%\Triade.o 
+echo Triade.cpp...
+g++ -c %SRC%\Triade.cpp -o %OBJ%\Triade.o 
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-    IF %ERRORLEVEL% EQU 0  (
+echo %NOME%.cpp...
+g++ -c %SRC%\%NOME%.cpp -o %OBJ%\%NOME%.o 
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-        echo linkando tudo...
-        g++ -Wextra -Werror %OBJ%\Nota.o %OBJ%\%NOME%.o %OBJ%\ObterNumero.o %OBJ%\BarraTitulo.o %OBJ%\Mathematics.o %OBJ%\Intervalo.o %OBJ%\Triade.o -o %OBJ%\%NOME%.exe        
+echo linkando tudo...
+g++ -Wextra -Werror      ^
+     %OBJ%\BarraTitulo.o ^
+     %OBJ%\Mathematics.o ^
+     %OBJ%\ObterNumero.o ^
+     %OBJ%\Nota.o        ^
+     %OBJ%\Intervalo.o   ^
+     %OBJ%\Triade.o      ^
+     %OBJ%\%NOME%.o      ^
+        -o %OBJ%\%NOME%.exe        
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-        IF %ERRORLEVEL% EQU 0  (
-            echo Executando...
-            %OBJ%\%NOME%.exe
-        )
-     )
-)
+echo Executando...
+%OBJ%\%NOME%.exe
+
