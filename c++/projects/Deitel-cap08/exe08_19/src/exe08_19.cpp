9abd50ac5059b75e4c64bbd45f659af389be5d39
marcuswlima <marcuswlima@gmail.com>
2023-07-11 01:28:02 -0300

    [c++/Deitel] Simpleton
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   c++/projects/Deitel-cap08/exe08_19/src/exe08_19.cpp
    	new file:   c++/readme.md

--- c++/projects/Deitel-cap08/exe08_19/src/exe08_19.cpp
+++ c++/projects/Deitel-cap08/exe08_19/src/exe08_19.cpp
@@ -2,59 +2,148 @@
 #include <iomanip>
 using namespace std;
 
-void dumpMemoria(int const * );
-void loadProgram(int * const , int * );
+//Protpripations------------------------
+void receberPrograma(int * const);
+void loadProgram(const int * const , int * );
+void run(int * const);
+void dumpMemoria(const int * const );
 
+//Globais--------------------------------------
+int registers[100][5];
+int gflagFim=-99999;
+
+//main--------------------------------------
 int main(){
-	int p1[10]={1007,1008,2007,3008,2109,1109,4300,0   ,0   ,0};
-	int p2[10]={1009,1010,2009,3010,4107,1109,4300,1110,4300,0};
+	cout << "*** Bem vindo ao Simpletron!                               ***\n";
+	cout << "*** Por favor insira uma instrução                         ***\n";
+	cout << "*** (ou data word) por vez em seu programa. Eu vou digitar ***\n";
+	cout << "*** o número de alocação e o ponto de interrogação (?).    ***\n";
+	cout << "*** Então você digita a palavra para a alocação.           ***\n";
+	cout << "*** Digite o número -99999 para parar de inserir dados     ***\n";
+	cout << "*** no seu programa.                                       ***\n";
+	cout << "**************************************************************\n";
+	cout << '\n'; 
+
+	int p[100];
 	int memory[100];
-	loadProgram(p1,memory);
+
+	receberPrograma(p);
+	loadProgram(p,memory);
+	run(memory);
 	dumpMemoria(memory);
-	loadProgram(p2,memory);
-//	dumpMemoria(memory);
 }//main
 
+
+void receberPrograma(int * const p){
+	int cont=0, digitado=0;
+	do{ 
+		cout << setfill('0') << setw(2) << cont << " ? ";
+		cin >> digitado;
+		p[cont] = digitado;
+		cont++;
+	}while (digitado != gflagFim);
+}//receberPrograma
+
 void dumpMemoria(int const * m) {
-	int value, resto;
 
 	//titulo
-	cout << '\t';
 	for (int x=0; x<=9; x++)
-		cout << setw(5) << right << x << '\t';
+		cout << x << right << '\t';
 	cout << '\n';
 
-	cout << setw(5) << '0' << '\t';
 	for (int x=0; x<=99; x++){
 		cout << '+';
-		value = m[x];
-		if (value>=1000)
-			cout <<	m[x];
-		else if (value >= 100)
-			cout <<	"0" << m[x];
-		else if (value >= 10)
-			cout <<	"00" << m[x];
-		else
-			cout <<	"000" << m[x];
 
-		resto = (x % 10);
-		if ( resto == 9 ){
-			cout << '\n';
-			if ((x/10)+1 < 10)
-				cout << setw(5) << ((x/10)+1)*10 << '\t';
-		}
-		else
-			cout << '\t';
-		
+		cout <<	setw(4) << setfill('0') << m[x];
+
+		cout << (x % 10 == 9 ? '\n' : '\t');
+	}
+	cout << '\n'; 
+
+	cout << "Registers:\n";
+	int i=0;
+	while (registers[i][0] != gflagFim){
+		cout << registers[i][0] << '\t' 
+			 << registers[i][1] << '\t'
+			 << registers[i][2] << '\t'
+			 << registers[i][3] << '\t'
+			 << registers[i][4] << '\t'
+			 ;
+		cout << '\n';
+		i++;
 	}
 }
 
-void loadProgram(int * const  p, int  * m){
+void loadProgram(const int * const p, int * m){
+	int i=0;
+	
 	for (int x=0; x<=99; x++)
 		m[x]=0;
 	
-	for( int i=0; i<=9; i++ )
+	while (p[i] != gflagFim){
 		m[i] = p[i];
-	
+		i++;
+	}
 }
 
+void run(int * const m){
+	int instrRegister, operationCode, operand, accumulator=0, instrCount=0, ate=10;
+	do{
+
+		instrRegister = m[instrCount];
+		operationCode = instrRegister/100;
+		operand = instrRegister % 100;
+
+		registers[instrCount][0]=instrCount;
+		registers[instrCount][1]=instrRegister;
+		registers[instrCount][2]=operationCode;
+		registers[instrCount][3]=operand;
+		
+		if (operationCode==43)
+			break;
+
+		switch (operationCode){
+			case 10:
+				cout << "Digite um número: ";
+				cin >> m[operand];
+				break;
+			case 11:
+				cout << "Resultado: " << m[operand] << '\n';
+				break;
+			case 20:
+				accumulator = m[operand];
+				break;
+			case 21:
+				m[operand] = accumulator;
+				break;
+			case 30:
+				accumulator += m[operand];
+				break;
+			case 31:
+				accumulator -= m[operand];
+				break;
+		}
+
+		if (operationCode < 40)
+			instrCount++;
+		else{
+			int unidade = operationCode % 10;
+
+			if (unidade==0)
+				instrCount = operand;
+			else if (unidade == 1 && accumulator < 0)
+				instrCount = operand;
+			else if (unidade == 2 && accumulator == 0)
+				instrCount = operand;
+			else 
+				instrCount++;
+		}
+
+		registers[instrCount][4]=accumulator;
+		
+	}while (instrCount<=ate);
+
+
+	registers[instrCount][0]=gflagFim;
+
+}
---
+++ c++/readme.md
@@ -0,0 +1,3 @@
+## Exemplos
+
+https://media.pearsoncmg.com/ph/esm/deitel/cpp_htp_8/code_examples/code_examples.html
