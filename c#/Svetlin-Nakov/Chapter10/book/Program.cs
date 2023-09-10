40748504da433c7f9546fd9c39762063c040de72
marcuswlima <marcuswlima@gmail.com>
2023-09-10 14:00:14 -0300

    commit

---
+++ c#/Svetlin-Nakov/Chapter10/book/Program.cs
@@ -0,0 +1,17 @@
+﻿using Svetlin_Nakov;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Cat someCat = new();
+        someCat.SayMiau();
+        Console.WriteLine("The color of cat {0} is {1}.",someCat.Name, someCat.Color);
+
+        Cat someCat2 = new("Johnny", "brown");
+        someCat2.SayMiau();
+        Console.WriteLine("The color of cat {0} is {1}.",
+        someCat2.Name, someCat.Color);
+    }
+}
